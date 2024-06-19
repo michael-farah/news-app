80c6b4d511194038621ea0fd4bbdb7e62edbc53a
michael-farah <michaelfarah@live.co.uk>
2024-06-19 16:12:52 +0100

    refactor: use loader to fetch data for routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,55 @@
-import { useEffect, useState } from "react";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import api from "../api";
-import Homepage from "./pages/home-page";
-import ErrorPage from "./pages/error-page";
-import ArticlePage from "./pages/article-page";
-import Layout from "./components/layout";
-import { CircularProgress } from "@mui/material";
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ArticleDetails from "./components/ArticleDetails";
+import ArticleList from "./components/ArticleList";
+import NavBarWrapper from "./components/NavBarWrapper";
+import {
+  fetchArticles,
+  fetchArticleById,
+  fetchCommentsByArticleId,
+} from "../api";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <NavBarWrapper />,
+    children: [
+      {
+        path: "/",
+        element: <ArticleList />,
+        loader: async () => {
+          const articles = await fetchArticles();
+          return articles;
+        },
+      },
+      {
+        path: "/articles",
+        element: <ArticleList />,
+        loader: async () => {
+          const articles = await fetchArticles();
+          return articles;
+        },
+      },
+      {
+        path: "/articles/:articleId",
+        element: <ArticleDetails />,
+        id: "article-details",
+        loader: async ({ params }) => {
+          const article = await fetchArticleById(params.articleId);
+          const comments = await fetchCommentsByArticleId(params.articleId);
+          return { article, comments };
+        },
+      },
+    ],
+  },
+]);
 
 function App() {
-  const [articles, setArticles] = useState([]);
-  const [votes, setVotes] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
-
-  const fetchArticles = async () => {
-    try {
-      const { data } = await api.getArticles();
-      setArticles(data);
-      const initialVotes = data.reduce((acc, article) => {
-        acc[article.article_id] = article.votes;
-        return acc;
-      }, {});
-      setVotes(initialVotes);
-    } catch (error) {
-      console.error("Failed to fetch articles", error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchArticles();
-  }, [fetchArticles]);
-
-  const handleVote = async (articleId, increment) => {
-    try {
-      await api.patchVotes(articleId, increment);
-      setVotes((prevVotes) => ({
-        ...prevVotes,
-        [articleId]: prevVotes[articleId] + increment,
-      }));
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  if (isLoading) {
-    return <CircularProgress aria-busy aria-label="Loading" />;
-  }
-
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />,
-      errorElement: <ErrorPage />,
-      children: [
-        {
-          index: true,
-          element: (
-            <Homepage
-              articles={articles}
-              votes={votes}
-              handleVote={handleVote}
-            />
-          ),
-        },
-        {
-          path: "articles",
-          element: (
-            <Homepage
-              articles={articles}
-              votes={votes}
-              handleVote={handleVote}
-            />
-          ),
-        },
-        {
-          path: "articles/:article_id",
-          element: (
-            <ArticlePage
-              articles={articles}
-              votes={votes}
-              handleVote={handleVote}
-            />
-          ),
-        },
-      ],
-    },
-  ]);
-
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
