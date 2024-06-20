65703cbc1c9dfab11f1a67aa7819442ead836036
michael-farah <michaelfarah@live.co.uk>
2024-06-20 13:28:15 +0100

    refactor(CommentCard.js): replace card stack component with Material UI card header component. - Implement API call to get user data for rendering Avatar images.

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,17 +1,51 @@
-import { Card, CardContent, Stack, Typography, Avatar } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Typography,
+  Avatar,
+} from "@mui/material";
 import timeSince from "../utils/timeSince";
+import api from "../../api";
+import { useEffect, useState } from "react";
 
-const CommentCard = ({ comment }) => (
-  <Card>
-    <CardContent>
-      <Stack spacing={2} direction="row" alignItems="center">
-        <Avatar alt={comment.author}>{comment.author.charAt(0)}</Avatar>
-        <Typography fontWeight="bold">{comment.author}</Typography>
-        <Typography>{timeSince(comment.created_at)}</Typography>
-      </Stack>
-      <Typography sx={{ p: "20px 0" }}>{comment.body}</Typography>
-    </CardContent>
-  </Card>
-);
+const CommentCard = ({ comment }) => {
+  const [userDetails, setUserDetails] = useState(null);
+
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      const users = await api.fetchUsers();
+      const userDetails = users.find(
+        (user) => user.username === comment.author,
+      );
+      setUserDetails(userDetails);
+    };
+
+    fetchUserDetails();
+  }, [comment.author]);
+
+  return (
+    <Card>
+      <CardHeader
+        avatar={
+          userDetails ? (
+            <Avatar
+              alt={comment.author}
+              src={userDetails.avatar_url}
+              style={{ border: "0.1px lightgrey solid" }}
+            />
+          ) : (
+            <Avatar />
+          )
+        }
+        title={comment.author}
+        subheader={timeSince(comment.created_at)}
+      />
+      <CardContent>
+        <Typography sx={{ p: "20px 0" }}>{comment.body}</Typography>
+      </CardContent>
+    </Card>
+  );
+};
 
 export default CommentCard;
