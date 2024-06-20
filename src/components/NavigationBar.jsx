3a6db9858cac10d1d94c6a9d25309fb3c820e481
michael-farah <michaelfarah@live.co.uk>
2024-06-20 13:39:28 +0100

    feat(NavigationBar): add user avatar and login/logout button to the navigation bar component

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,9 +1,36 @@
-import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
+import { useUser } from "../contexts/UserContext";
+import LoginForm from "./LoginForm";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Avatar,
+  IconButton,
+  Grid,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useMediaQuery } from "@mui/material";
+import { useEffect } from "react";
+import { getUserFromCookie, setUserInCookie } from "../utils/cookieUtils";
 
 function NavigationBar() {
+  const { user, loginOpen, setLoginOpen, setUser } = useUser();
+  const handleLoginOpen = () => setLoginOpen(true);
+  const handleLoginClose = () => setLoginOpen(false);
+  const handleLogout = () => {
+    setUser(null);
+    setUserInCookie(null);
+    setLoginOpen(false);
+  };
   const isMobile = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+
+  useEffect(() => {
+    const savedUser = getUserFromCookie();
+    if (savedUser) {
+      setUser(savedUser);
+    }
+  }, [setUser]);
+
   return (
     <nav>
       <AppBar position="static" color="primary">
@@ -18,10 +45,41 @@ function NavigationBar() {
               <MenuIcon />
             </IconButton>
           )}
-          <Typography variant="h6" color="inherit" noWrap>
-            News App
-          </Typography>
+          <Grid container alignItems="center">
+            <Typography variant="h6" color="inherit" noWrap>
+              News App
+            </Typography>
+          </Grid>
+          <Grid container justifyContent="flex-end">
+            <IconButton onClick={handleLoginOpen}>
+              {user ? (
+                <>
+                  <Typography variant="h6" color="white">
+                    {user.username}
+                  </Typography>
+                  <Avatar
+                    sx={{ ml: 2 }}
+                    src={user.avatar_url}
+                    style={{ border: "0.1px lightgrey solid" }}
+                  />
+                </>
+              ) : (
+                <>
+                  <Typography variant="h6" color="white">
+                    Login
+                  </Typography>
+                  <Avatar sx={{ ml: 2 }} />
+                </>
+              )}
+            </IconButton>
+          </Grid>
         </Toolbar>
+        <LoginForm
+          open={loginOpen}
+          onClose={handleLoginClose}
+          submitCookie={setUserInCookie}
+          logout={handleLogout}
+        />
       </AppBar>
     </nav>
   );
