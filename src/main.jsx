544adcf1187e1477fa903b99e82d15c314d174f6
michael-farah <michaelfarah@live.co.uk>
2024-06-21 13:29:23 +0100

    style: change theme colour to match logo

--- src/main.jsx
+++ src/main.jsx
@@ -2,36 +2,40 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { indigo } from "@mui/material/colors";
 
 const theme = createTheme({
   palette: {
+    contrastThreshold: 4.5,
     primary: {
-      main: '#1976d2',
+      main: indigo[700],
     },
     secondary: {
-      main: '#ff4081',
+      main: indigo[300],
     },
     background: {
-      default: '#f5f5f5',
+      default: "#f5f5f5",
+    },
+    text: {
+      primary: "#000000",
+      secondary: "#4f4f4f",
     },
   },
   typography: {
     fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
     h1: {
-      fontSize: '2.5rem',
+      fontSize: "2.5rem",
       fontWeight: 500,
     },
     h2: {
-      fontSize: '2rem',
+      fontSize: "2rem",
       fontWeight: 500,
     },
     body1: {
-      fontSize: '1rem',
+      fontSize: "1rem",
     },
   },
 });
-
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
